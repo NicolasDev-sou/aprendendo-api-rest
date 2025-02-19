fac0645e25e4b3cc54908254125322a02c0b2031
NIcolas Assis <outisnicolas@gmail.com>
2025-02-19 09:24:16 -0300

    adicionando recurso de estilos de codigo

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const server = express();
 
 server.get('/', (_req: Request, res: Response) => {
-    res.send('E ai meu patrao!');
+    res.send('E ai meu patrao! como vai?');
 });
 
 export { server };
